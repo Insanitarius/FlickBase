20472338c1b17aa96968cc978d25bf1f9008e16e
Insanitarius <prototype.shubhodeep@gmail.com>
2021-08-09 20:13:47 +0530

    User model finished

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -98,6 +98,39 @@ router.route("/isauth").get(checkLoggedIn, async (req, res) => {
   res.status(200).send(getUserProps(req.user));
 });
 
+router
+  .route("/update_email")
+  .patch(
+    checkLoggedIn,
+    grantAccess("updateOwn", "profile"),
+    async (req, res) => {
+      try {
+        if (await User.emailTaken(req.body.newemail)) {
+          return res.status(400).json({ message: "Sorry email already taken" });
+        }
+
+        const user = await User.findOneAndUpdate(
+          { _id: req.user._id, email: req.body.email },
+          {
+            $set: {
+              email: req.body.newemail,
+            },
+          },
+          { new: true }
+        );
+        if (!user) return res.status(400).json({ message: "User not found" });
+
+        const token = user.generateToken();
+        res
+          .cookie("x-access-token", token)
+          .status(200)
+          .send({ email: user.email });
+      } catch (error) {
+        res.status(400).json({ message: "Problem updating", error: error });
+      }
+    }
+  );
+
 const getUserProps = (user) => {
   return {
     _id: user._id,
