996bbffce1b5ea47e50ce5ffc4af7de5f5e22333
Insanitarius <prototype.shubhodeep@gmail.com>
2021-08-10 21:12:41 +0530

    Redux store added to fetch articles

---
+++ client/src/hoc/mainLayout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const MainLayout = (props) => {
+  return (
+    <Container className={`app_container mb-5`}>{props.children}</Container>
+  );
+};
+
+export default MainLayout;
--- client/src/routes.js
+++ client/src/routes.js
@@ -3,14 +3,17 @@ import { Switch, Route, BrowserRouter } from "react-router-dom";
 import GoogleFontLoader from "react-google-font-loader";
 import Home from "./components/home";
 import Header from "./components/navigation/header";
+import MainLayout from "./hoc/mainLayout";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Switch>
-        <Route path="/" component={Home} />
-      </Switch>
+      <MainLayout>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </MainLayout>
       <GoogleFontLoader
         fonts={[
           { font: "Roboto", weights: [300, 400, 900] },
