064140cbea0d2a884d895f8d9cf3082fcaa4866b
Insanitarius <prototype.shubhodeep@gmail.com>
2023-08-24 12:06:55 +0530

    fixed server file

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/api/articles", articles);
 
 if (process.env.NODE_ENV === "production") {
 	const path = require("path");
-	app.get("/*", (req, res) => {
+	app.get("/", (req, res) => {
 		app.use(express.static(path.resolve(__dirname, "client", "build")));
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
